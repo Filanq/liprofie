46fbedceac762bd5214865a39c924a2e1b6f0fc1
Filan <fedosenko009@mail.ru>
2024-10-28 00:33:09 +0300

    Prod

--- liprofie/settings.py
+++ liprofie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+#()*4+0#-^@rkd@_53^*q^kxnc2_q787^dmhc!blxt)*@2ba)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
